c592a48524f32540e2231c751a9789ec5a322b89
omeranha <53233626+omeranha@users.noreply.github.com>
2022-01-26 22:27:49 -0300

    hm

--- data/chatchannels/scripts/english_chat.lua
+++ data/chatchannels/scripts/english_chat.lua
@@ -1,5 +1,5 @@
 function onSpeak(player, type, message)
-	if player:getLevel() < 20 and not player:isPremium() then
+	if player:getLevel() < configManager.getNumber(configKeys.CHAT_LEVEL) and not player:isPremium() then
 		player:sendCancelMessage("You may not speak in this channel unless you have reached level 20 or your account has premium status.")
 		return false
 	end
